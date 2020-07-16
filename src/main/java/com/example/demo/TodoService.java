9dc9d1276c245e7ac8a113a3df4662e575b817b8
Marcus Hellberg <marcus@vaadin.com>
2020-07-16 10:56:39 -0700

    use database and add bean validation

--- src/main/java/com/example/demo/TodoService.java
+++ src/main/java/com/example/demo/TodoService.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
@@ -14,34 +12,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.SessionScope;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class TodoService {
-  private ArrayList<Todo> todos = new ArrayList<>();
+  private TodoRepository repo;
+
+  TodoService(TodoRepository repo) {
+    this.repo = repo;
+  }
 
   @PostConstruct
   @Scheduled(fixedRate = 5 * 60 * 1000)
   void init() {
-    todos.clear();
-    todos.add(new Todo("Build todo app"));
+    repo.deleteAll();
+    repo.save(new Todo("Build todo app"));
   }
 
   @GetMapping("/todos")
   public List<Todo> getTodos() {
-    return todos;
+    return repo.findAll();
   }
 
   @PostMapping(value = "/todos")
   public Todo addTodo(@RequestBody String task) {
-    Todo todo = new Todo(task);
-    todos.add(todo);
-    return todo;
+    return repo.save(new Todo(task));
   }
 
   @DeleteMapping("/todos/{id}")
-  public void deleteTodo(@PathVariable String id) {
-    todos.removeIf(t -> t.getId().equals(UUID.fromString(id)));
+  public void deleteTodo(@PathVariable Long id) {
+    repo.deleteById(id);
   }
 }
